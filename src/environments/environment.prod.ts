2495c5c271674610749a3de3be15f8f65dd8fef0
josevsousa <jose.vicente.de.sousa@gmail.com>
2023-11-18 11:50:58 -0300

    ionic ok

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDQu1fqg700s8IjnpvwH-Bj_Tdfh5f67Vo",
+    authDomain: "controlinspection-e57e2.firebaseapp.com",
+    projectId: "controlinspection-e57e2",
+    storageBucket: "controlinspection-e57e2.appspot.com",
+    messagingSenderId: "177383424944",
+    appId: "1:177383424944:web:3f0ff5d9c9dc45f1324c75"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDQu1fqg700s8IjnpvwH-Bj_Tdfh5f67Vo",
+    authDomain: "controlinspection-e57e2.firebaseapp.com",
+    projectId: "controlinspection-e57e2",
+    storageBucket: "controlinspection-e57e2.appspot.com",
+    messagingSenderId: "177383424944",
+    appId: "1:177383424944:web:3f0ff5d9c9dc45f1324c75"
+  }
 };
 
 /*
